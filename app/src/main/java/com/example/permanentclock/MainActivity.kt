e79d493c6427777476d0f16ecc1eefa6007ae96b
dshu <david.shu@disneystreaming.com>
2020-06-30 13:41:27 -0400

    removed Glide lib and made icons into local images

--- app/src/main/java/com/example/permanentclock/MainActivity.kt
+++ app/src/main/java/com/example/permanentclock/MainActivity.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.os.PowerManager
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import com.bumptech.glide.Glide
 import com.example.permanentclock.models.OWMWeatherResponse
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,7 +20,6 @@ import kotlin.math.roundToInt
 class MainActivity : AppCompatActivity() {
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-        const val IMG_URL = "https://openweathermap.org/img/wn/%s@2x.png"
         const val API_KEY = "5c0a9ca8d7fbd4165b728d3bddc1d281"
         const val CITY = "11375"
         const val UNITS = "imperial"
@@ -32,17 +31,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        wakeLock = (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
-            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::MyWakelockTag").apply {
-                acquire()
-            }
-        }
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE
 
         window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
             if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+                window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE
             }
         }
 
@@ -50,25 +46,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // setup hourly weather update
-        mHandler = Handler()
+        mHandler = Handler(Looper.getMainLooper())
         mRunnable = Runnable {
             getCurrentWeather()
-            mHandler.postDelayed(mRunnable, 3600000)
+            mHandler.postDelayed(mRunnable, 1800000) //3600000)
         }
         mHandler.post(mRunnable)
     }
 
-    override fun onStop() {
-        super.onStop()
-        wakeLock.release()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        wakeLock.acquire()
-    }
-
-    internal fun getCurrentWeather() {
+    private fun getCurrentWeather() {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -84,25 +70,22 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<OWMWeatherResponse>, t: Throwable) {
-                t.printStackTrace()
+                //t.printStackTrace()
             }
         })
 
     }
 
-    fun convertDouble(temp: Double): String {
+    private fun convertDouble(temp: Double): String {
         return temp.roundToInt().toString()
     }
 
-    fun updateWeatherUI(response: OWMWeatherResponse) {
+    private fun updateWeatherUI(response: OWMWeatherResponse) {
         val iconView: ImageView = findViewById(R.id.weatherIcon)
         val tempView: TextView = findViewById(R.id.weatherTemp)
-
-        val imgUrl = String.format(IMG_URL, response.weather[0].icon)
-        Glide.with(this)
-            .load(imgUrl)
-            .error(R.drawable.placeholder)
-            .into(iconView)
+        val imgResource = String.format("wi_%s_2x", response.weather[0].icon)
+        val res = resources.getIdentifier(imgResource, "drawable", packageName)
+        iconView.setImageResource(res)
         val detailsFormat = "%s\u00b0 (%s°)   H: %s%%   W: %s"
         tempView.text = String.format(
             detailsFormat,
