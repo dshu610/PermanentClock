a23e9fd3dda5f16bf2e09d8757670783eedb1a10
dshu <david.shu@disneystreaming.com>
2020-07-09 20:26:27 -0400

    added reset settings feature

--- app/src/main/java/com/example/permanentclock/SettingsActivity.kt
+++ app/src/main/java/com/example/permanentclock/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.permanentclock
 
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.preference.PreferenceManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -20,6 +22,36 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            // add reset settings button and alert dialog
+            val resetButton: Preference? = findPreference("reset")
+            resetButton?.setOnPreferenceClickListener {
+                val builder = AlertDialog.Builder(requireActivity())
+                builder.setMessage(R.string.reset_message)
+                    .setPositiveButton(
+                        R.string.reset_yes_btn
+                    ) { dialog, which ->
+                        PreferenceManager.getDefaultSharedPreferences(activity).edit()
+                            .clear().commit()
+                        PreferenceManager.setDefaultValues(
+                            activity?.baseContext,
+                            R.xml.root_preferences,
+                            true
+                        )
+
+                        // refresh settings screen
+                        preferenceScreen.removeAll()
+                        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+                    }
+                    .setNegativeButton(
+                        R.string.reset_no_btn
+                    ) { dialog, which -> }
+                builder.create()
+                builder.show()
+                true
+            }
+
+
         }
     }
 }
